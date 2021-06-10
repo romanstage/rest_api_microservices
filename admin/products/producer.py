4db538c8579d5ac4b3ab5405cfb4557f74bfbe8e
codelooper75 <deuscode787@gmail.com>
2021-06-10 18:48:28 +0300

    main service added, rabbitmq is setup

---
+++ admin/products/producer.py
@@ -0,0 +1,13 @@
+import pika, json
+import config
+
+params = pika.URLParameters(config.AMPQ_URL)
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='main', body=json.dumps(body), properties=properties)
